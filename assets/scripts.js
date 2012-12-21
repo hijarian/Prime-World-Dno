83971e4628de2f4379b14055a7d298fc8e3da26c
Saphronov Mark <hijarian@gmail.com>
2012-12-21 19:58:02 +0400

    Удалил ненужный код, который делал глупый баг.

--- assets/scripts.js
+++ assets/scripts.js
@@ -26,9 +26,6 @@ function makeChartWithData (data) {
 	subtitle:{
 	    text:'Source: http://ru.playpw.com/ratings.html'
 	},
-	xAxis:{
-	    categories:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-	},
 	yAxis:{
 	    title:{
 		text:'Rating'
