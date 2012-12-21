1cd239235811513b27e0220c1f848b13e8e5367d
Saphronov Mark <hijarian@gmail.com>
2012-12-21 15:12:09 +0400

    Теперь данные с сайта преобразовываются в формат, приемлемый для Highcharts.

--- src/dno.lisp
+++ src/dno.lisp
@@ -5,6 +5,75 @@
 (defparameter *ratings-xpath* 
   "//div[@class='b-ratinglist__i2']/div[contains(concat(' ', @class, ' '), ' b-ratings ')]")
 
+(defparameter *class-names*
+  (make-hash-table
+   :test #'equal
+   :size 100)
+  "A hash defining mapping between hero class codes and hero class names")
+(setf (gethash "duel" *class-names*) "Дуэлянт")
+(setf (gethash "krio" *class-names*) "Крио")
+(setf (gethash "faceless" *class-names*) "Безликий")
+(setf (gethash "warloard" *class-names*) "Воевода")
+(setf (gethash "molnienosnyi" *class-names*) "Молниеносный")
+(setf (gethash "ten" *class-names*) "Тень")
+(setf (gethash "eger" *class-names*) "Егерь")
+(setf (gethash "izobretatel" *class-names*) "Изобретатель")
+(setf (gethash "gorec" *class-names*) "Горец")
+(setf (gethash "combat" *class-names*) "Комбат")
+(setf (gethash "firefox" *class-names*) "Огненная лиса")
+(setf (gethash "hiler" *class-names*) "Целительница")
+(setf (gethash "night" *class-names*) "Царица ночи")
+(setf (gethash "gora" *class-names*) "Человек-гора")
+(setf (gethash "chist" *class-names*) "Чистильщик")
+(setf (gethash "deva" *class-names*) "Дева")
+(setf (gethash "strelok" *class-names*) "Стрелок")
+(setf (gethash "znecdush" *class-names*) "Жнец душ")
+(setf (gethash "krisolov" *class-names*) "Крысолов")
+(setf (gethash "luchnica" *class-names*) "Лучница")
+(setf (gethash "klik" *class-names*) "Клык")
+(setf (gethash "zhaba" *class-names*) "Жабий наездник")
+(setf (gethash "vedun" *class-names*) "Ведун")
+(setf (gethash "charozmey" *class-names*) "Чарозмей")
+(setf (gethash "muza" *class-names*) "Муза")
+(setf (gethash "master" *class-names*) "Мастер клинков")
+(setf (gethash "zaklinatel" *class-names*) "Заклинатель")
+(setf (gethash "fiksy" *class-names*) "Фикси")
+(setf (gethash "witcherb" *class-names*) "Ведьмак")
+(setf (gethash "dokhtrina" *class-names*) "Доктрина")
+(setf (gethash "prince-thief" *class-names*) "Принц воров")
+(setf (gethash "viuga" *class-names*) "Вьюга")
+(setf (gethash "white-mask" *class-names*) "Белая маска")
+(setf (gethash "predvoditel-adore" *class-names*) "Предводитель")
+(setf (gethash "gromoverjec" *class-names*) "Громовержец")
+(setf (gethash "nevidimka" *class-names*) "Невидимка")
+(setf (gethash "tancuyuschii-s-volkami" *class-names*) "Танцующий с волками")
+(setf (gethash "hudoshnica" *class-names*) "Художница")
+(setf (gethash "bessmertny" *class-names*) "Бессмертный")
+(setf (gethash "mejin" *class-names*) "Мейдзин")
+(setf (gethash "redhvost" *class-names*) "Рыжий Хвост")
+(setf (gethash "grica" *class-names*) "Жрица")
+(setf (gethash "pantera" *class-names*) "Чёрная пантера")
+(setf (gethash "rokot" *class-names*) "Рокот")
+(setf (gethash "asasin" *class-names*) "Ассасин")
+(setf (gethash "nimfa" *class-names*) "Нимфа")
+(setf (gethash "ohotnik" *class-names*) "Охотник")
+(setf (gethash "dushelov" *class-names*) "Душелов")
+(setf (gethash "povelitelkris" *class-names*) "Повелитель крыс")
+(setf (gethash "amazonka" *class-names*) "Амазонка")
+(setf (gethash "kogot" *class-names*) "Коготь")
+(setf (gethash "bolotcaras" *class-names*) "Болотный Царь")
+(setf (gethash "lesovik" *class-names*) "Лесовик")
+(setf (gethash "magozavr" *class-names*) "Магозавр")
+(setf (gethash "bard" *class-names*) "Бард")
+(setf (gethash "otmechennij" *class-names*) "Отмеченный Змеем")
+(setf (gethash "charodey" *class-names*) "Чародей")
+(setf (gethash "koroleva_fey" *class-names*) "Королева фей")
+(setf (gethash "witchera" *class-names*) "Ведьмак")
+(setf (gethash "demonolog" *class-names*) "Демонолог")
+
+(defun make-human-readable-hero-class (hero-class)
+  (or (gethash hero-class *class-names*) hero-class))
+
 (defun get-hero-class (subnode)
   (string-trim " "
                (cl-ppcre:regex-replace-all "b-ratings-?" 
@@ -14,18 +83,21 @@
 (defun get-rating-for-player (subnode)
   "Accepts the DOM element with data about single player standing, player name and rating"
   (list 
-   (xpath:find-string subnode "./b")
-   (xpath:find-string subnode "./em")
-   (xpath:find-string subnode "./span")))
+   (cons "name" (xpath:find-string subnode "./em[contains(concat(' ', @class, ' '), ' nickname ')]"))
+   (cons "x"    (parse-integer (xpath:find-string subnode "./b")))
+   (cons "y"    (parse-integer (remove-if-not #'alphanumericp (xpath:find-string subnode "./span"))))))
 
 (defun get-ratings-for-hero (node)
   "Accepts the DOM element with data about all ratings for a given hero"
-  (cons (get-hero-class node) 
-        (map-on-node-by-xpath node "./ul/li" #'get-rating-for-player)))
-
+  (let ((hero-class (get-hero-class node)))
+    (if (not (or (equal hero-class "h-common") (equal hero-class "h-doct") (equal hero-class "h-adorn")))
+        (list
+         (cons "name" (make-human-readable-hero-class hero-class))
+         (cons "data" (map-on-node-by-xpath node "./ul/li" #'get-rating-for-player))))))
+    
 (defun collect-ratings (html-page)
   "From the raw HTML text gets ratings list"
-  (map-on-xpath html-page *ratings-xpath* #'get-ratings-for-hero))
+  (remove-if #'null (map-on-xpath html-page *ratings-xpath* #'get-ratings-for-hero)))
 
 
 ;; MAIN 
