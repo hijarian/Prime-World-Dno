a710c381cad62abfc2f7ada6ebc7f97c5c09a2b8
Saphronov Mark <hijarian@gmail.com>
2013-06-06 09:25:41 +0400

    Added explanatory comments to heroku-setup.lisp

--- heroku-setup.lisp
+++ heroku-setup.lisp
@@ -4,12 +4,13 @@
 
 (print ">>> Building system....")
 
+; Notice that *build-dir* is a pathname, not a bare string, so we have to use `merge-pathnames` here.
 (load (merge-pathnames *build-dir* "dno.asd"))
 
 (ql:quickload :dno)
 
-(dno:run)
+; We DO NOT evaluate (dno:run) here, because all it does is start a Hunchentoot instance and Heroku CL buildpack already does this itself.
 
-;;; Redefine / extend heroku-toplevel here if necessary.
+; Redefine / extend heroku-toplevel here if necessary.
 
 (print ">>> Done building system")
